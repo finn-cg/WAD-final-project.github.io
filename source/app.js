c26db61c5e601c23a4c6dacd21338b375bd45664
hip-nncuong162 <63356009+hip-nncuong162@users.noreply.github.com>
2021-11-18 17:36:27 +0700

    final

--- source/app.js
+++ source/app.js
@@ -1,13 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const listRouter = require('./routes/list');
+const detailRouter = require('./routes/detail');
+const signInRouter = require('./routes/signIn');
+const signUpRouter = require('./routes/signUp');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +24,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/list', listRouter);
+app.use('/detail', detailRouter);
 app.use('/users', usersRouter);
+app.use('/signIn', signInRouter);
+app.use('/signUp', signUpRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
